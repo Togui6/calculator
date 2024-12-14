3949c3e5ab816a9320e45142b27dcaa6183cf6d5
nhat <nhatguillon@gmail.com>
2024-12-14 16:08:47 +1100

    Create screen display and update screen display

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let first = 0;
+let second = 0;
+let operator = "caca";
+
+const screenDisplay = document.querySelector("#display");
+const displayed = document.createElement("div");
+displayed.textContent = operator;
+displayed.style.color = "blue";
+screenDisplay.appendChild(displayed);
+
+
+
 function add(a, b) {
 return a + b;
 }
@@ -12,4 +24,13 @@ return a * b;
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(a, b) {
+return a + operator + b;
+}
+
+function updateScreenDisplay() {
+displayed.textContent = operator;
+displayed.style.color = "blue";
 }
